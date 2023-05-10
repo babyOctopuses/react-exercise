6c2d01827e0b4ff9a25e909188c0d75da71256fa
Julfendi <babyoctopuses@gmail.com>
2023-05-10 15:08:42 +0700

    button function

--- exercise1/src/Navbar/Home.js
+++ exercise1/src/Navbar/Home.js
@@ -1,9 +1,21 @@
 import React from 'react'
 
+const handleClick = () =>{
+    console.log("test");
+}
+
+const handleClickAgain = (name) => {
+    console.log("hello "+name);
+} 
+
 function Home() {
   return (
-    <div>
-    <p>Home</p>
+    <div className='home'>
+        <h2>Homepage</h2>
+        <button onClick={handleClick}>Click Me</button>
+        <button onClick={()=>
+            handleClickAgain("Jul")
+        }>Click Me again</button>
     </div>
   )
 }
