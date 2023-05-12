4f95df1302e13bf2ec15ad5160f96f0be994f77c
Julfendi <babyoctopuses@gmail.com>
2023-05-12 08:34:58 +0700

    added .map

--- exercise1/src/Navbar/Home.js
+++ exercise1/src/Navbar/Home.js
@@ -1,34 +1,26 @@
 import React from 'react'
 import { useState } from 'react';
 
- 
-
-
-function Home() {
+ function Home() {
   
-    const [name, setName]=useState("mario");
-
-    const handleClick = () =>{
-        if(name=="mario"){
-            setName("luigi")
-        } else {
-            setName("mario")
-        }
-        
-    }
-
-    const handleClickAgain = (name, e) => {
-        console.log("hello" , e.target);
-    }
+    const [blogs, setBlogs]=useState([
+        {title:"Book 1", author: "author 1", body:"body 1", id:1},
+        {title:"Book 2", author: "author 2", body:"body 2", id:2},
+        {title:"Book 3", author: "author 3", body:"body 3", id:3}
+    ]);
 
     return (
     <div className='home'>
         <h2>Homepage</h2>
-        <p>{name}</p>
-        <button onClick={handleClick}>Click Me</button>
-        <button onClick={(e)=>
-            handleClickAgain("Jul", e)
-        }>Click Me again</button>
+        <div className='home'>
+            {blogs.map((blog)=>(
+                <div className="blog-preview" key={blog.id}>
+                    <h2>{blog.title}</h2>
+                    <p>by {blog.author}</p>
+                </div>
+            )
+            )}
+        </div>
     </div>
   )
 }
