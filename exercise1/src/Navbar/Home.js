1dd3292468dc90d0c91dbe447572086b88cec1ff
Julfendi <babyoctopuses@gmail.com>
2023-05-12 09:20:49 +0700

    added reusable component

--- exercise1/src/Navbar/Home.js
+++ exercise1/src/Navbar/Home.js
@@ -5,15 +5,16 @@ import BlogList from '../BlogList';
  function Home() {
   
     const [blogs, setBlogs]=useState([
-        {title:"Book 1", author: "author 1", body:"body 1", id:1},
-        {title:"Book 2", author: "author 2", body:"body 2", id:2},
-        {title:"Book 3", author: "author 3", body:"body 3", id:3}
+        {title:"Book 1", author: "yoshi", body:"body 1", id:1},
+        {title:"Book 2", author: "mario", body:"body 2", id:2},
+        {title:"Book 3", author: "mario", body:"body 3", id:3}
     ]);
 
     return (
     <div className='home'>
         <h2>Homepage</h2>
         <BlogList blogs={blogs} title="All Titles"/>
+        <BlogList blogs={blogs.filter((blog)=>blog.author === "mario")} title="Mario's list"/>
     </div>
   )
 }
