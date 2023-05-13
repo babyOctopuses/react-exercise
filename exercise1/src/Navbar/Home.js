b4ad01328ac6d93d1de807d5a467d922ec8b7d4e
Julfendi <babyoctopuses@gmail.com>
2023-05-13 07:58:09 +0700

    added custom hook

--- exercise1/src/Navbar/Home.js
+++ exercise1/src/Navbar/Home.js
@@ -1,34 +1,11 @@
 import React from 'react'
 import { useState, useEffect } from 'react';
 import BlogList from '../BlogList';
+import useFetch from '../useFetch';
 
  function Home() {
 
-    const [blogs, setBlogs]=useState(null);
-    const [isPending, setIsPending] = useState(true);
-    const [error, setError] = useState(null);
-
-    useEffect(()=>{
-        setTimeout(()=>{
-            fetch("http://localhost:8000/blogss")
-                .then(res =>{
-                    if(!res.ok){
-                        throw Error("could not fetch data from the resource");
-                    }
-                    return res.json();
-                })
-                .then(data=>{
-                    setBlogs(data);
-                    setIsPending(false);
-                    setError(null);
-                })
-                .catch(err=>{
-                    setIsPending(false);
-                    setError(err.message);
-                })
-        }, 1000) 
-    },[])
-
+    const {data :  blogs, isPending, error} = useFetch('http://localhost:8000/blogs');
 
     return (
     <div className='home'>
