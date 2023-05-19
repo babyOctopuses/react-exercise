b68197883e64afb72294875f8719b2ef9a01aff0
Julfendi <babyoctopuses@gmail.com>
2023-05-19 09:11:25 +0700

    added input field

--- exercise1/src/Navbar/Create.js
+++ exercise1/src/Navbar/Create.js
@@ -1,8 +1,41 @@
-import React from 'react'
+import React, { useState } from 'react';
 
 const Create = () => {
+
+    const [title, setTitle]= useState();
+    const [body, setBody]= useState();
+    const [author, setAuthor]= useState('mario');
+
   return (
-    <div>create</div>
+    <div className="create">
+        <h2>Add a new blog</h2>
+        <form>
+        <label>Blog title:</label>
+        <input
+            type= "text"
+            required
+            value={title}
+            onChange={(e)=>{setTitle(e.target.value)}}
+        />
+        <label>Blog body:</label>
+        <input
+            type= "text"
+            required
+            value={body}
+            onChange={(e)=>{setBody(e.target.value)}}
+        />
+        <label>Author</label>
+        <select
+            required
+            value={author}
+            onChange={(e)=>{setAuthor(e.target.value)}}
+        >
+            <option value= "mario">mario</option>
+            <option value= "yoshi">yoshi</option>
+            </select>
+            <button> Add blog </button>
+            </form>
+    </div>
   )
 }
 
